e85923e196c4112bcfe82717ae3e674d2aa1b4e3
nirasan <niranirao@gmail.com>
2018-11-28 18:25:30 +0900

    update main

--- go-k8s/main.go
+++ go-k8s/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request: %#v", r)
 		fmt.Fprintf(w, "Hello %s", r.URL.Query().Get("name"))
 	})
 	log.Fatal(http.ListenAndServe(":18080", nil))
